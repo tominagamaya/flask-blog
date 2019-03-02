f023f5cd0984244b50d056ac64372ed731b667af
tominagamaya <maya.tominaga.9@gmail.com>
2019-03-02 20:21:07 +0900

    認証機能を追加

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'cfb33786023cc152019e747a051f73c6'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
